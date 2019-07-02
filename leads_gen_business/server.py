715a8502de14af9a74d9b9f5e6c268e2a0e275a7
Ian Aranha <ianorama@gmail.com>
2019-07-01 22:30:33 -0700

    Adding 'leads_gen_business'

--- leads_gen_business/server.py
+++ leads_gen_business/server.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, require, redirect
+from flask import Flask, render_template, request, redirect
 from mysqlconnection import connectToMySQL
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    mysql = connectToMySQL('lead_get_business')
+    mysql = connectToMySQL('lead_gen_business')
     all_clients = mysql.query_db('SELECT * FROM clients')
     return render_template('index.html', clients = all_clients)
 
