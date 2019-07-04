9513e641615c6d618aad74cb3806f6fb5f4f9f61
Ian Aranha <ianorama@gmail.com>
2019-07-04 12:41:32 -0700

    Most of the project completed. have not figured out how to graph data yet. moving on.

--- leads_gen_business/server.py
+++ leads_gen_business/server.py
@@ -8,7 +8,6 @@ def index():
     mysql = connectToMySQL('lead_gen_business')
     all_clients = mysql.query_db(
         "SELECT CONCAT(clients.first_name,' ', clients.last_name) AS 'Name', COUNT(leads.leads_id) AS 'Leads' FROM clients JOIN sites ON sites.client_id = clients.client_id JOIN leads ON leads.site_id = sites.site_id GROUP BY CONCAT(clients.first_name,' ', clients.last_name)")
-    print (all_clients)
     return render_template('index.html', clients=all_clients)
 
 
